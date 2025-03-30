4e9134ab92ec10e1d99c7d5ad2d940107a8ce065
Harald Barth <haba@kth.se>
2025-03-30 21:54:27 +0200

    copyright note and other comments

--- Sniffer.h
+++ Sniffer.h
@@ -1,3 +1,21 @@
+/*
+ *  © 2025 Harald Barth
+ *  
+ *  This file is part of CommandStation-EX
+ *
+ *  This is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  It is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CommandStation.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #ifdef ARDUINO_ARCH_ESP32
 #include <Arduino.h>
 #include "driver/mcpwm.h"
@@ -25,8 +43,11 @@ public:
   };
   inline DCCPacket fetchPacket() {
     noInterrupts();
+    // if dcclen = 0 which means that there
+    // is no new data, this will create a
+    // packet with length 0 (which is no packet)
     DCCPacket d(dcclen, dccbytes);
-    dcclen = 0;
+    dcclen = 0; // reset (data has been fetched)
     interrupts();
     return d;
   };
