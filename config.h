955924045bcd804f70797fa8e40f9c00ba6dfae1
Harald Barth <haba@kth.se>
2025-12-29 12:35:47 +0100

    Add config.h and myAutomation.h

---
+++ config.h
@@ -0,0 +1,19 @@
+#define ENABLE_WIFI true
+#define IP_PORT 2560
+#define WIFI_HOSTNAME "dccex-damnloop"
+#define WIFI_CHANNEL 11
+//#include "config.example.h"
+
+//#undef WIFI_FORCE_AP
+//#define WIFI_FORCE_AP true
+
+#include "config.STACKEN.h"
+//#include "config.SLAMRA.h"
+//#define DIAG_IO
+#define MAX_CURRENT 1500       // take it easy
+//#define SCOPE
+#define DCC_TURNOUTS_RCN_213   // compat to Digitrax on home layout
+
+#undef MOTOR_SHIELD_TYPE
+#define MOTOR_SHIELD_TYPE STANDARD_MOTOR_SHIELD
+
